5db940cbbc27fa64a5a9414565b38240eb1b0d0f
14justin <justinmwangi40@gmail.com>
2024-04-03 20:29:53 +0300

    test

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ function addingEventListener(){
     const input = document.getElementById('button');
      input.addEventListener('click', function() {
        alert('I was clicked!');
-});
-
-}
-const input = document.getElementById('button');
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
+  });
+} 
